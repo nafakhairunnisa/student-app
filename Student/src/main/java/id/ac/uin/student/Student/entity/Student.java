f1a2e3a38a0c82bb31cca28ed42818a789acf344
nafakhairunnisa <83491188+nafakhairunnisa@users.noreply.github.com>
2022-03-21 19:31:38 +0700

    Student App with Postman

---
+++ Student/src/main/java/id/ac/uin/student/Student/entity/Student.java
@@ -0,0 +1,35 @@
+package id.ac.uin.student.Student.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotEmpty
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotEmpty
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotEmpty
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+}
